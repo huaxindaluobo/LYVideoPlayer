8518e6b8814d7cf2fa52bb67b1de627420969c5e
萝卜 <17710616626@163.com>
2020-02-13 13:17:15 +0800

    Initial Commit

---
+++ LYVideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LYVideoPlayer
+//
+//  Created by 刘洋 on 2020/2/13.
+//  Copyright © 2020 佳缘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LYVideoPlayerTests/LYVideoPlayerTests.swift
@@ -0,0 +1,34 @@
+//
+//  LYVideoPlayerTests.swift
+//  LYVideoPlayerTests
+//
+//  Created by 刘洋 on 2020/2/13.
+//  Copyright © 2020 佳缘. All rights reserved.
+//
+
+import XCTest
+@testable import LYVideoPlayer
+
+class LYVideoPlayerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
