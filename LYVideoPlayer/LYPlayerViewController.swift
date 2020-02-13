ea2a54e279bb69a50d4f0b74b5a5de8747556837
萝卜 <17710616626@163.com>
2020-02-13 18:14:42 +0800

    add videoVC

---
+++ LYVideoPlayer/LYPlayerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LYPlayerViewController.swift
+//  LYVideoPlayer
+//
+//  Created by 刘洋 on 2020/2/13.
+//  Copyright © 2020 佳缘. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class LYPlayerViewController: LYBaseViewController {
+
+    var playUrl = String()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
